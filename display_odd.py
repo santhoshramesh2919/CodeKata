97617758ca11e884acb22692d2d1060b84866d83
Santhosh Kumar R <46440736+santhoshramesh2919@users.noreply.github.com>
2019-01-08 12:14:34 +0530

    Update display_odd.py

--- display_odd.py
+++ display_odd.py
@@ -1,5 +1,11 @@
 #odd nos
+l=[]
 n,q=map(int,input().split())
 for i in range(n+1,q):
   if i%2==1:
-    print(i,end=" ")
+    l.append(i)
+for j in range(0,len(l)):
+  if j==len(l)-1:
+    print(l[j])
+  else:
+    print(l[j],end=" ")
