84081b5bba9e8fcada0cf9950a49b85d247425c5
Santhosh Kumar R <46440736+santhoshramesh2919@users.noreply.github.com>
2019-01-08 15:14:34 +0530

    Update sum_of_AP.py

--- sum_of_AP.py
+++ sum_of_AP.py
@@ -2,9 +2,23 @@
 n,a,d=map(int,input().split(" "))
 sum =a
 for i in range(1,n):
-  li.append(a+d)
   a=a+d
   sum=sum+a
 print(sum)
-
   
+#arithmetic progression
+2
+n,a,d=map(int,input().split(" "))
+3
+sum =a
+4
+for i in range(1,n):
+5
+  li.append(a+d)
+6
+  a=a+d
+7
+  sum=sum+a
+8
+print(sum)
+9
