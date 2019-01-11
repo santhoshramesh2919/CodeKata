0966a88c6905c86d5f34c896473a1bbd5c5de9ee
Santhosh Kumar R <46440736+santhoshramesh2919@users.noreply.github.com>
2019-01-11 17:34:18 +0530

    Update fib.py

--- fib.py
+++ fib.py
@@ -4,8 +4,11 @@ n1=1
 n2=1
 count=0
 while count < n:
-       print(n1,end=' ')
-       nth = n1 + n2
-       n1 = n2
-       n2 = nth
-       count += 1
+       if count==n-1:
+              print(n1)
+       else:
+              print(n1,end=' ')
+              nth = n1 + n2
+              n1 = n2
+              n2 = nth
+              count += 1
