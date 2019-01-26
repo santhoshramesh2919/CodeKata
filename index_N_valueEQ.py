7f8a2af33529a0cc4183a556e8ad5ac5a5590b9a
Santhosh Kumar R <46440736+santhoshramesh2919@users.noreply.github.com>
2019-01-26 17:39:19 +0530

    Update index_N_valueEQ.py

--- index_N_valueEQ.py
+++ index_N_valueEQ.py
@@ -1,9 +1,12 @@
-#finding the index and value equivalences
+#finding the largest combination
 n=int(input())
 li=list(input().split(" "))
 li1=[]
 for i in range(0,len(li)):
     if int(li[i])==i:
         li1.append(li[i])
-li1.sort()
-print(" ".join(li1))
+if (len(li)!=0):
+    li1.sort()
+    print(" ".join(li1))
+else:
+    print("-1")
