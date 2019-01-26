74304b681f414e2e39cebba02d9c5168a0835c1a
Santhosh Kumar R <46440736+santhoshramesh2919@users.noreply.github.com>
2019-01-26 17:45:07 +0530

    Update index_N_valueEQ.py

--- index_N_valueEQ.py
+++ index_N_valueEQ.py
@@ -5,8 +5,8 @@ li1=[]
 for i in range(0,len(li)):
     if int(li[i])==i:
         li1.append(li[i])
-if (len(li)!=0):
+if (len(li1)==0):
+    print("-1")    
+else:    
     li1.sort()
     print(" ".join(li1))
-else:
-    print("-1")
