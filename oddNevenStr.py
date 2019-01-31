78078287f949848030157c16c733557d9498bf4a
Santhosh Kumar R <46440736+santhoshramesh2919@users.noreply.github.com>
2019-01-31 06:08:54 +0530

    Create oddNevenStr.py

---
+++ oddNevenStr.py
@@ -0,0 +1,10 @@
+#ODd strings and even strings separated
+str=input()
+strodd=""
+streven=""
+for i in range(0,len(str)):
+    if i%2==0:
+        strodd=strodd+str[i]
+    else:
+        streven=streven+str[i]
+print(strodd,streven
