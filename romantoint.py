52bc0bd971e9aae539484818a03efe6b151d4cbc
Santhosh Kumar R <46440736+santhoshramesh2919@users.noreply.github.com>
2019-01-31 16:14:55 +0530

    Create romantoint.py

---
+++ romantoint.py
@@ -0,0 +1,6 @@
+n=input()
+li=['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV','XV','XVI','XVII','XVIII','XIX','XX']
+for i in range(0,len(li)):
+    if li[i]==n:
+        print(i+1)
+        
